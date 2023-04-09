734c31f6d79239fdee12cd3ab0245c0013e100b2
hwenjun <66753253+hhhwenjun@users.noreply.github.com>
2023-04-09 12:59:19 -0700

    add flyweight

---
+++ src/Flyweight.java
@@ -0,0 +1,11 @@
+
+public class Flyweight extends AirObject {
+    
+    public Flyweight() {
+        super();
+    }
+    
+    public Flyweight(int xOrig, int yOrig, int zOrig, int xWidth, int yWidth, int zWidth, int level) {
+        super(xOrig, yOrig, zOrig, xWidth, yWidth, zWidth, level);
+    }
+}
